6ce26ef8631c2b782bb033cfe1f99fa25fdd62b6
Anjali-st <anjali90908080@gmail.com>
2023-09-08 13:10:57 +0530

    VariousActionResults

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assis_04VariousActionResult.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
